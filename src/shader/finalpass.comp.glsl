e165fed13682d300d6e7c6e4a78b9f53eb7828a2
Vladimir Semyonov <tempname011@gmail.com>
2021-08-27 12:07:28 +0300

    fix edge pixel bug

--- src/shader/finalpass.comp.glsl
+++ src/shader/finalpass.comp.glsl
@@ -3,6 +3,6 @@ layout(binding = 0, rgba16) uniform image2D final_image;
 layout(binding = 1) uniform sampler2D lbuffer_image;
 
 void main() {
-  vec3 light = texture(lbuffer_image, (vec2(gl_WorkGroupID.xy) - 0.5) / gl_NumWorkGroups.xy).rgb;
+  vec3 light = texture(lbuffer_image, (vec2(gl_WorkGroupID.xy) + 0.5) / gl_NumWorkGroups.xy).rgb;
   imageStore(final_image, ivec2(gl_WorkGroupID.xy), vec4(2.0 * light, 1.0));
 }
