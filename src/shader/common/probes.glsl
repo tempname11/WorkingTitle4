ede8f5dab4b387b8f1ed1ede021daabb831c0252
Vladimir Semyonov <tempname011@gmail.com>
2021-09-27 13:42:55 +0300

    add probe helper functions

--- src/shader/common/probes.glsl
+++ src/shader/common/probes.glsl
@@ -4,7 +4,8 @@ float madfrac(float a, float b) {
   return a * b - floor(a * b);
 }
 
-// originally from DDGI sample code at http://jcgt.org/published/0008/02/01/
+// originally from DDGI sample code.
+// see http://jcgt.org/published/0008/02/01/
 vec3 spherical_fibonacci(float i, float n) {
   float phi = 2.0 * PI * madfrac(i, PHI - 1.0);
   float cos_theta = 1.0 - (2.0 * i + 1.0) / n;
@@ -16,4 +17,34 @@ vec3 spherical_fibonacci(float i, float n) {
     sin(phi) * sin_theta,
     cos_theta
   );
+}
+
+float sign_not_zero(float k) {
+  return k >= 0.0 ? 1.0 : -1.0;
+}
+
+vec2 sign_not_zero(vec2 v) {
+  return vec2(
+    sign_not_zero(v.x),
+    sign_not_zero(v.y)
+  );
+}
+
+// see http://jcgt.org/published/0003/02/01/
+vec2 octo_encode(vec3 v) {
+  float l1norm = abs(v.x) + abs(v.y) + abs(v.z);
+  vec2 result = v.xy / l1norm;
+  if (v.z < 0.0) {
+    result = (1.0 - abs(result.yx)) * sign_not_zero(result.xy);
+  }
+  return result;
+}
+
+// see http://jcgt.org/published/0003/02/01/
+vec3 octo_decode(vec2 o) {
+  vec3 v = vec3(o.x, o.y, 1.0 - abs(o.x) - abs(o.y));
+  if (v.z < 0.0) {
+    v.xy = (1.0 - abs(v.yx)) * sign_not_zero(v.xy);
+  }
+  return normalize(v);
 }
