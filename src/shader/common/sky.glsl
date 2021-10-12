829fb8b8c50498cc713f65483466aa496658231d
Vladimir Semyonov <tempname011@gmail.com>
2021-10-12 11:42:26 +0300

    replace 'borrowed' sky with quick ad hoc sky

--- src/shader/common/sky.glsl
+++ src/shader/common/sky.glsl
@@ -3,148 +3,20 @@
 
 #include "constants.glsl"
 
-#define iSteps 16
-#define jSteps 8
-
-// see https://github.com/wwwtyro/glsl-atmosphere/blob/master/index.glsl
-
-vec2 rsi(vec3 r0, vec3 rd, float sr) {
-    // ray-sphere intersection that assumes
-    // the sphere is centered at the origin.
-    // No intersection when result.x > result.y
-    float a = dot(rd, rd);
-    float b = 2.0 * dot(rd, r0);
-    float c = dot(r0, r0) - (sr * sr);
-    float d = (b*b) - 4.0*a*c;
-    if (d < 0.0) return vec2(1e5,-1e5);
-    return vec2(
-        (-b - sqrt(d))/(2.0*a),
-        (-b + sqrt(d))/(2.0*a)
-    );
-}
-
-vec3 atmosphere(vec3 r, vec3 r0, vec3 pSun, float iSun, float rPlanet, float rAtmos, vec3 kRlh, float kMie, float shRlh, float shMie, float g) {
-    // Normalize the sun and view directions.
-    pSun = normalize(pSun);
-    r = normalize(r);
-
-    // Calculate the step size of the primary ray.
-    vec2 p = rsi(r0, r, rAtmos);
-    if (p.x > p.y) return vec3(0,0,0);
-    p.y = min(p.y, rsi(r0, r, rPlanet).x);
-    float iStepSize = (p.y - p.x) / float(iSteps);
-
-    // Initialize the primary ray time.
-    float iTime = 0.0;
-
-    // Initialize accumulators for Rayleigh and Mie scattering.
-    vec3 totalRlh = vec3(0,0,0);
-    vec3 totalMie = vec3(0,0,0);
-
-    // Initialize optical depth accumulators for the primary ray.
-    float iOdRlh = 0.0;
-    float iOdMie = 0.0;
-
-    // Calculate the Rayleigh and Mie phases.
-    float mu = dot(r, pSun);
-    float mumu = mu * mu;
-    float gg = g * g;
-    float pRlh = 3.0 / (16.0 * PI) * (1.0 + mumu);
-    float pMie = 3.0 / (8.0 * PI) * ((1.0 - gg) * (mumu + 1.0)) / (pow(1.0 + gg - 2.0 * mu * g, 1.5) * (2.0 + gg));
-
-    // Sample the primary ray.
-    for (int i = 0; i < iSteps; i++) {
-
-        // Calculate the primary ray sample position.
-        vec3 iPos = r0 + r * (iTime + iStepSize * 0.5);
-
-        // Calculate the height of the sample.
-        float iHeight = length(iPos) - rPlanet;
-
-        // Calculate the optical depth of the Rayleigh and Mie scattering for this step.
-        float odStepRlh = exp(-iHeight / shRlh) * iStepSize;
-        float odStepMie = exp(-iHeight / shMie) * iStepSize;
-
-        // Accumulate optical depth.
-        iOdRlh += odStepRlh;
-        iOdMie += odStepMie;
-
-        // Calculate the step size of the secondary ray.
-        float jStepSize = rsi(iPos, pSun, rAtmos).y / float(jSteps);
-
-        // Initialize the secondary ray time.
-        float jTime = 0.0;
-
-        // Initialize optical depth accumulators for the secondary ray.
-        float jOdRlh = 0.0;
-        float jOdMie = 0.0;
-
-        // Sample the secondary ray.
-        for (int j = 0; j < jSteps; j++) {
-
-            // Calculate the secondary ray sample position.
-            vec3 jPos = iPos + pSun * (jTime + jStepSize * 0.5);
-
-            // Calculate the height of the sample.
-            float jHeight = length(jPos) - rPlanet;
-
-            // Accumulate the optical depth.
-            jOdRlh += exp(-jHeight / shRlh) * jStepSize;
-            jOdMie += exp(-jHeight / shMie) * jStepSize;
-
-            // Increment the secondary ray time.
-            jTime += jStepSize;
-        }
-
-        // Calculate attenuation.
-        vec3 attn = exp(-(kMie * (iOdMie + jOdMie) + kRlh * (iOdRlh + jOdRlh)));
-
-        // Accumulate scattering.
-        totalRlh += odStepRlh * attn;
-        totalMie += odStepMie * attn;
-
-        // Increment the primary ray time.
-        iTime += iStepSize;
-
-    }
-
-    // Calculate and return the final color.
-    return iSun * (pRlh * kRlh * totalRlh + pMie * kMie * totalMie);
-}
-
 vec3 sky(vec3 ray, vec3 sun_direction, vec3 sun_illuminance) {
-  // Lots of `earth-like` constants.
-  vec3 result = atmosphere(
-    normalize(ray.xzy),
-    vec3(0.0, 6e6, 0.0),
-    sun_direction.xzy,
-    22.0,
-    6e6,
-    61e5,
-    vec3(5.5e-6, 13.0e-6, 22.4e-6),
-    21e-6,
-    8e3,
-    1.2e3,
-    0.758
-  ) * (
-    length(sun_illuminance) / sqrt(75.0) // @Hack
+  // Ad hoc stuff that looks okay.
+
+  vec3 result = (
+    (vec3(0.2, 0.2, 0.9) + 0.05 * ray)
+      * sun_illuminance / 5.0
+      * (1.5 + dot(ray, sun_direction))
+    + vec3(1.0)
+      * sun_illuminance / 2.0
+      * pow(
+        max(0.0, dot(ray, sun_direction)),
+        50.0
+      )
   );
-  
-  // Subtle hint at orientation for debugging.
-  result = ray * 0.1 + result;
-
-  // Get rid of ugly cutout at nadir.
-  float nadir_weight = clamp(
-    dot(ray, vec3(0, 0, -1.0)) / 0.05,
-    0.0,
-    1.0
-  );
-  result = mix(result, vec3(0.0), nadir_weight);
-
-  if (any(isnan(result)) || any(isinf(result))) {
-    return vec3(0.0);
-  }
-
   return result;
 }
 
