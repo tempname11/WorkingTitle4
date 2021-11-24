b58098167b18766f68a0246ed67150a147ac3ef0
Vladimir Semyonov <tempname011@gmail.com>
2021-11-24 15:43:05 +0300

    tweak shadows

--- src/shader/lpass_sun.frag.glsl
+++ src/shader/lpass_sun.frag.glsl
@@ -27,7 +27,7 @@ const float Z_FAR = 10000.0;
 
 const float RAY_T_MIN_MULT = 0.01;
 const float RAY_T_MAX = 10000.0;
-const float SHADOW_NORMAL_BIAS_MULT = 0.001;
+const float SHADOW_NORMAL_BIAS_MULT = 0.01;
 
 void main() {
   float depth = subpassLoad(zchannel).r;
@@ -54,7 +54,7 @@ void main() {
       0,
       0xFF,
       pos_world + N * SHADOW_NORMAL_BIAS_MULT * length(pos_view),
-      0, // RAY_T_MIN_MULT * length(pos_view), // try to compensate for Z precision.
+      0,
       -directional_light.direction,
       RAY_T_MAX
     );
